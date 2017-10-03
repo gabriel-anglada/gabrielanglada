781ee0a7ee0a57eebae8245e0239bc12f5c2236d
Gaston Zorzano <gaston.zorzano.st@everis.com>
2017-10-03 09:39:40 +0200

    Global pos changes

---
+++ src/app/components/global-position-accounts/global-position-accounts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-position-accounts',
+  templateUrl: './global-position-accounts.component.html',
+  styleUrls: ['./global-position-accounts.component.scss']
+})
+
+export class GlobalPositionAccountsComponent implements OnInit {
+  unica = 124673.89;
+  freedom = 124673.89;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/global-position-cards/global-position-cards.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'global-position-cards',
+  templateUrl: './global-position-cards.component.html',
+  styleUrls: ['./global-position-cards.component.scss']
+})
+
+export class GlobalPosCardsComponent {
+  tarjeta1 = 2855;
+  tarjeta2 = 2855;
+
+}

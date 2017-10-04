b93921249b688ba22c7deb60b11d296696a0d8ef
Gaston Zorzano <gaston.zorzano.st@everis.com>
2017-10-04 10:18:31 +0200

    Cards opt design ready, lacks proper data model, for the time being is just a dummy card class model, carousel works, displays information based on centered card, lacks a view to show in case there’s no cards to show

--- src/app/pages/cards/cards.component.ts
+++ src/app/pages/cards/cards.component.ts
@@ -7,68 +7,55 @@ import { trigger, state, transition, animate, style } from '@angular/animations'
   templateUrl: 'cards.component.html',
   styleUrls: ['./cards.component.scss'],
   animations: [
-          trigger('desplegado', [
-                            state('default', style({ transform: 'rotate(0)' })),
-                            state('rotated', style({ transform: 'rotate(-180deg)' })),
-                            transition('rotated => default', animate('400ms ease-out')),
-                            transition('default => rotated', animate('400ms ease-in'))
-                                ]),
-          trigger('contentDeploy', [
-                            state('default', style({height: '0'})),
-                            state('expanded', style({height: '*'})),
-                            transition('expanded => default', animate('600ms ease')),
-                            transition('default => expanded', animate('600ms ease'))
-                                  ])
-              ]
+    trigger('desplegado', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-180deg)' })),
+      transition('rotated => default', animate('400ms ease-out')),
+      transition('default => rotated', animate('400ms ease-in'))
+    ]),
+    trigger('contentDeploy', [
+      state('default', style({height: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded => default', animate('600ms ease')),
+      transition('default => expanded', animate('600ms ease'))
+    ])
+  ]
 })
 export class CardsPageComponent implements OnInit {
+  // "Listener", when this changes, information currently displayed changes
   @Input() private currentCard: ClientCardModel;
 
   private cards: Array<ClientCardModel>;
-  private indexCard = 0;
 
-  //for animation trigger
+  //for animation triggers
   private state = 'default';
   private content = 'default';
 
-//default value
+  //default value, remove once proper data has been provided
   private price = 37.45;
 
   constructor() {}
-
+  
   ngOnInit() {
     this.cards = [
       new ClientCardModel("1243 5432 6532 9357", "Pau Roger Garcia", 1234, 13434, 14324, 342341, "assets/bml-visa-debito-1.png"),
-      new ClientCardModel("1243 7245 2364 3245", "Marc Mikel Garcia", 75671, 4321, 15435, 2343241, "assets/bml-visa-debito-1.png"),
-      new ClientCardModel("1243 5451 4773 2435", "Roger Ferran Garcia", 5451, 1565, 65641, 123432, "assets/bml-visa-debito-1.png"),
-      new ClientCardModel("1243 3253 4654 2647", "Mikel Carles Garcia", 23431, 5461, 345341, 234321, "assets/bml-visa-debito-1.png"),
-      new ClientCardModel("1243 1343 3455 2145", "Ariel Alex Garcia", 1756756, 65461, 15345, 22341, "assets/bml-visa-debito-1.png"),
-      new ClientCardModel("1243 1234 7764 4324", "Pepe Machinegun Garcia", 1345345, 12323, 23421, 234431, "assets/bml-visa-debito-1.png"),
+      new ClientCardModel("7566 7245 2364 3245", "Marc Mikel Garcia", 75671, 4321, 15435, 2343241, "assets/bml-visa-debito-1.png"),
+      new ClientCardModel("3453 5451 4773 2435", "Roger Ferran Garcia", 5451, 1565, 65641, 123432, "assets/bml-visa-debito-1.png"),
+      new ClientCardModel("8775 3253 4654 2647", "Mikel Carles Garcia", 23431, 5461, 345341, 234321, "assets/bml-visa-debito-1.png"),
+      new ClientCardModel("3423 1343 3455 2145", "Ariel Alex Garcia", 1756756, 65461, 15345, 22341, "assets/bml-visa-debito-1.png"),
+      new ClientCardModel("0898 1234 7764 4324", "Pepe Machinegun Garcia", 1345345, 12323, 23421, 234431, "assets/bml-visa-debito-1.png"),
     ];
     this.currentCard = this.cards[0];
   }
 
-//swipe events for the carousel
-  swipeLeft() { //preguntar como hacer el ternario
-    if(this.indexCard >= (this.cards.length - 1)) {
-      this.indexCard = this.cards.length -1;
-    } else {
-      this.indexCard++;
-    }
-    this.currentCard = this.cards[this.indexCard];
-  }
-  swipeRight() {
-    if(this.indexCard <= 0) {
-      this.indexCard = 0;
-    } else {
-      this.indexCard--;
-    }
-    this.currentCard = this.cards[this.indexCard];
+  //event fired method when user swipes
+  userSwiped(ev){
+    this.currentCard = this.cards[ev.activeIndex];
   }
 
-  //animation state changer method
+  //animation state changer method for collapsible component
   desplegar() {
-        this.state = (this.state === 'default' ? 'rotated' : 'default');
-        this.content = (this.content === 'default' ? 'expanded' : 'default');
-    }
+    this.state = (this.state === 'default' ? 'rotated' : 'default');
+    this.content = (this.content === 'default' ? 'expanded' : 'default');
+  }
 }

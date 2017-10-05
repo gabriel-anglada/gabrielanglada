4b31792343b965e2c5e09e2bf5f0ff7a1c3fb03d
Gaston Zorzano <gaston.zorzano.st@everis.com>
2017-10-05 11:52:25 +0200

    Commit before merge

--- src/app/components/componet-library/component-library.component.ts
+++ src/app/components/componet-library/component-library.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 
 export class ComponentLibrary {
+  private showAlert = false;
   //card component
   private cardNumber = "1234 1234 1234 1234";
   private cardAmount = 1234.56;
@@ -22,7 +23,7 @@ export class ComponentLibrary {
     {price: 1234.12, name: "Pull Bear", company: "Arteixo"},
     {price: 4341.23, name: "Pull Cat", company: "Arteixo"},
     {price: 4344.43, name: "Pull Dog", company: "Arteixo"},
-  ]
+  ];
 
   //account lines
   private accounts = [
@@ -31,5 +32,9 @@ export class ComponentLibrary {
     {date: "3 May", name: "Suministros Luz - Endesa S.L. Corporation", balance: 12.56, total: 12867 },
     {date: "3 May", name: "Suministros Luz - Endesa S.L. Corporation", balance: -12.56, total: 12867 },
     {date: "3 May", name: "Suministros Luz - Endesa S.L. Corporation", balance: 12.56, total: 12867 }
-  ]
+  ];
+
+  alert() {
+    this.showAlert = this.showAlert ? false : true;
+  }
 }
